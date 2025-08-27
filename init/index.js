183280d204d93838d0fd791a491e79155b321cf6
Vishal Maurya <mauryavishal9919@gmail.com>
2025-08-27 10:29:56 +0530

    fix Mongo

--- init/index.js
+++ init/index.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
