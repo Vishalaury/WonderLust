7f349ba34946fe57701e6c82b991329598c8e840
Vishal Maurya <mauryavishal9919@gmail.com>
2025-08-27 10:37:51 +0530

    fix /listings

--- app.js
+++ app.js
@@ -120,9 +120,7 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.get("/", (req, res) => {
-  res.redirect("/listings");
-});
+
 
 // 404 handler
 app.all(/.*/, (req, res, next) => {
